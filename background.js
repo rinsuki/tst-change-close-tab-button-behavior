d1325da19b9e82dba81eaf2239e65ed5e598e29b
rinsuki <428rinsuki+git@gmail.com>
2019-05-09 23:17:36 +0900

    fix bug

--- background.js
+++ background.js
@@ -1,30 +1,35 @@
 const TST_ID="treestyletab@piro.sakura.ne.jp"
 
 async function main() {
+
     browser.runtime.onMessageExternal.addListener(async (message, sender) => {
         if (sender.id !== TST_ID) return
         console.log(message)
-        if (message.type !== "tab-clicked") return
-        if (message.closebox !== true) return
-        let tabIds = [message.tab.id]
-        if (message.button === 1) {
-            // すべての子孫タブを閉じる
-            function loop(childrens) {
-                for (const childTab of childrens) {
-                    tabIds.push(childTab.id)
-                    loop(childTab.children)
+        switch (message.type) {
+        case "tab-clicked":
+            if (message.closebox !== true) return
+            let tabIds = [message.tab.id]
+            if (message.button === 1) {
+                // すべての子孫タブを閉じる
+                function loop(childrens) {
+                    for (const childTab of childrens) {
+                        tabIds.push(childTab.id)
+                        loop(childTab.children)
+                    }
                 }
+                loop(message.tab.children)
             }
-            loop(message.tab.children)
+            await browser.tabs.remove(tabIds)
+            return true
+        case "ready":
+            await browser.runtime.sendMessage(TST_ID, {
+                type: "register-self",
+                name: browser.runtime.getManifest().name,
+                icons: browser.runtime.getManifest().icons,
+                listeningTypes: ["tab-clicked"]
+            })
+            break
         }
-        await browser.tabs.remove(tabIds)
-        return true
-    })
-    const success = await browser.runtime.sendMessage(TST_ID, {
-        type: "register-self",
-        name: browser.runtime.getManifest().name,
-        icons: browser.runtime.getManifest().icons,
-        listeningTypes: ["tab-clicked"]
     })
 }
 main()
